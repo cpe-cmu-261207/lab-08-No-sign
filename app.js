fd7173f556f41e1ca6d2befe96b751a68decdfe6
No-sign <55203083+No-sign@users.noreply.github.com>
2021-03-15 14:15:38 +0700

    create bugs

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const fs = require("fs");
-
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var body_parser_1 = require("body-parser");
+var fs_1 = require("fs");
+var app = express_1["default"]();
+app.use(body_parser_1["default"].json());
 //to post you must use bodyParser
-
-app.use(express.static("assets"));
-
-app.get("/", (req, res) => {
-  res.end(fs.readFileSync("./instruction.html"));
+app.get("/", function (req, res) {
+    res.end(fs_1["default"].readFileSync("./instruction.html"));
 });
-
 //implement your api here
 //follow instruction in http://localhost:8000/
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`server started on port:${port}`));
+var port = process.env.PORT || 8000;
+app.listen(port, function () { return console.log("server started on port:" + port); });
